c4182ba77da5095cd2486f1995f81b4856e52a91
arin <arinjuliaanneh@gmail.com>
2025-11-25 23:04:37 -0400

    + main menu + menu logic

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Hospital_Level");
+      
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
